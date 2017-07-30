43b94c1df5746d51f1c324079e6c49271f5f579f
신승륜 <ssr@sinseunglyun-ui-MacBook-Pro.local>
2017-07-30 20:15:58 +0900

    제발..

--- app/models/app_note.rb
+++
@@ -1,3 +0,0 @@
-class AppNote < Notification
-
-end
--- app/models/info.rb
+++
@@ -1,8 +0,0 @@
-class Info < ApplicationRecord
-  belongs_to :user
-  belongs_to :appointment
-
-  def find_builder
-    appointment.infos.find_by(builder: true)
-  end
-end

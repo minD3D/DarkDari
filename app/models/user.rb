2189e1229f77fbc7954998ff5601c157a0b641ca
신승륜 <ssr@sinseunglyun-ui-MacBook-Pro.local>
2017-08-17 14:28:40 +0900

    idea야 사라져라

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :nickname, length: { in: 3..10 }
   validates :phone, format: { with: /01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})/,
                               message: "휴대폰 번호가 아닙니다"}
+  before_destroy :destroy_appointments
 
   has_many :infos, dependent: :destroy
   has_many :appointments, through: :infos
@@ -14,12 +15,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # TODO: 유저가 사라질 때 빌더인 약속은 지워지고, 그냥 참여만 한 약속은 남아있게 하기
-  # before_destroy do
-  #   self.infos.where(builder: true).each do |info|
-  #     info.appointment.destroy
-  #   end
-  # end
+  # destroy appointments whose builder is current user
+  def destroy_appointments
+    self.infos.where(builder: true).each do |info|
+      info.appointment.destroy
+    end
+  end
 
   def self.search(search)
     if search.present?
